a3c282c783f3cb68190379a09b604488acb9ff73
ginpei <ginpei@gmail.com>
2019-10-14 10:43:46 -0700

    remove dummy items

--- src/screens/HomePage.tsx
+++ src/screens/HomePage.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import BasicLayout from '../complexes/BasicLayout';
 import firebase from '../middleware/firebase';
@@ -7,37 +7,10 @@ import { isAdmin, useUser } from '../models/users';
 import { BasicHeading1 } from '../pure/BasicHeading';
 import LoadingScreen from './LoadingScreen';
 
-const NoteItem: FC<{ note: any }> = (props) => {
-  const { note } = props;
-  const title = note.title || (note.body as string).split('\n')[0];
-  return (
-    <li>{title}</li>
-  );
-};
-
 const HomePage: React.FC = () => {
-  const [notesInitialized, setNotesInitialized] = useState(false);
   const [user, userInitialized] = useUser(firebase.auth());
-  const [notes, setNotes] = useState<any[]>([]);
 
-  useEffect(() => {
-    return firebase.firestore().collection('free-notes').onSnapshot({
-      next: (snapshot) => {
-        const dataList = snapshot.docs.map((doc) => ({
-          ...doc.data(),
-          id: doc.id,
-        }));
-        setNotes(dataList);
-        setNotesInitialized(true);
-      },
-      error: (error) => {
-        console.error(error);
-        setNotesInitialized(true);
-      },
-    });
-  }, []);
-
-  if (!userInitialized || !notesInitialized) {
+  if (!userInitialized) {
     return (
       <LoadingScreen />
     );
@@ -67,9 +40,6 @@ const HomePage: React.FC = () => {
         <li>
           <Link to={getConferencePath('list')}>Active Conferences</Link>
         </li>
-        {notes.map((note) => (
-          <NoteItem key={note.id} note={note} />
-        ))}
       </ul>
     </BasicLayout>
   );
